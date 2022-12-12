e5e92b5b720268b83aab75a8a1c33b9cbede4d04
jpssantiago <jps_santiago@outlook.com>
2022-12-12 00:47:17 -0300

    feat: kindacode .csv example

--- app/lib/pages/main/main_page.dart
+++ app/lib/pages/main/main_page.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List<List<dynamic>> _data = [];
+
+  void _loadCSV() async {
+    final rawData = await rootBundle.loadString("assets/kindacode.csv");
+    List<List<dynamic>> listData = const CsvToListConverter().convert(rawData);
+    setState(() {
+      _data = listData;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Projeto de I.A'),
+      appBar: AppBar(
+        title: const Text("Kindacode.com"),
+      ),
+      body: ListView.builder(
+        itemCount: _data.length,
+        itemBuilder: (_, index) {
+          return Card(
+            margin: const EdgeInsets.all(3),
+            color: index == 0 ? Colors.amber : Colors.white,
+            child: ListTile(
+              leading: Text(_data[index][0].toString()),
+              title: Text(_data[index][1]),
+              trailing: Text(_data[index][2].toString()),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loadCSV,
+        child: const Icon(Icons.add),
       ),
     );
   }
